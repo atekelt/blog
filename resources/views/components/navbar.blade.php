e295c9262c88ed3a336b0fc19dba4d61b378d0ff
atekelt <atekeltafework55@gmail.com>
2025-04-04 02:34:11 +0300

    drake view fix

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,14 +5,14 @@
         <li><a href="{{ url('/') }}">Home</a></li>
         <li><a href="{{ url('/about') }}">About</a></li>
         @auth
+            <li><a href="{{ route('files.create') }}">Upload</a></li>
+            <li><a href="{{ route('profile.edit') }}">Profile</a></li>
             <li>
-                <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                <a href="#" class="text-danger" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                 <form id="logout-form" method="POST" action="{{ route('logout') }}" style="display: none;">
                     @csrf
                 </form>
             </li>
-            <li><a href="{{ route('files.create') }}">Upload</a></li>
-            <li><a href="{{ route('profile.edit') }}">Profile</a></li>
         @else
             <li><a href="{{ url('/login') }}">Login</a></li>
         @endauth
--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -9,10 +9,10 @@
         </p>
     </header>
 
-    <x-danger-button
+    <button type="submit" class="py-2 px-4 rounded text-danger"
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+    >{{ __('Delete Account') }}</button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
@@ -46,9 +46,9 @@
                     {{ __('Cancel') }}
                 </x-secondary-button>
 
-                <x-danger-button class="ms-3">
+                <button type="submit" class="py-2 px-4 rounded text-danger">
                     {{ __('Delete Account') }}
-                </x-danger-button>
+                </button>
             </div>
         </form>
     </x-modal>
